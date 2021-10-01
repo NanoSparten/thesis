a19b18db5179ed2c0e620015305ed90c89e4ae09
Noah Gardner <ngngardner@gmail.com>
2021-10-01 15:10:11 -0400

    julia notebook with lbp

--- flake.nix
+++ flake.nix
@@ -5,28 +5,54 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils = { url = "github:numtide/flake-utils"; };
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    devshell-flake = { url = "github:numtide/devshell"; };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils, flake-compat, devshell-flake }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          devShell = pkgs.mkShell {
-            packages = [
-              # paper dev
-              (pkgs.texlive.combine {
-                inherit (pkgs.texlive)
-                  scheme-small
-                  latexmk
-                  latexindent
-                  # extra tex packages go here
-                  biblatex
-                  ;
-              })
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              devshell-flake.overlay
             ];
           };
+        in
+        {
+          devShell = with pkgs;  devshell.mkShell
+            {
+              packages = [
+                julia_16-bin
+
+                # paper dev
+                (pkgs.texlive.combine {
+                  inherit (pkgs.texlive)
+                    scheme-small
+                    latexmk
+                    latexindent
+
+                    # extra tex packages go here
+                    biblatex
+                    ;
+                })
+              ];
+
+              commands = [{
+                name = "pluto";
+                category = "Julia";
+                command = ''
+                  eval $(echo "julia -e 'import Pkg; Pkg.activate(\".\"); using Pluto; Pluto.run()'")
+                '';
+                help = "launch pluto server";
+              }];
+
+              env = [
+                {
+                  name = "JULIA_PROJECT";
+                  value = ".";
+                }
+              ];
+            };
         });
 }
